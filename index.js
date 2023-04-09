2f87f9819f2c6519bb16bb37f0f7ceb518ac6963
Shubhkarman Singh Rathore <shubhkarman4733@yahoo.in>
2023-04-08 18:31:18 -0700

    adds moving things lab

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Your code here
+// const dodger = document.getElementById('dodger');
+// dodger.style.background = '#FF69B4';
+// dodger.style.bottom = '0px';
+// dodger.style.left = '0px';
+
+const dodger = document.getElementById("dodger");
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+
+  function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+    
+    if (left < 360) {
+      dodger.style.left = `${left + 1}px`;
+    }
+  }
+  
+  document.addEventListener("keydown", function (event) {
+    if (event.key === "ArrowLeft") {
+      moveDodgerLeft();
+    } else if (event.key === "ArrowRight") {
+      moveDodgerRight();
+    }
+  });
